ec4c347e7a4b47602fba765d00193e719a859307
Just a pixel <91799709+ri-Soul@users.noreply.github.com>
2022-09-07 17:43:23 +0200

    last - update

--- HexaDecimal/Dec-Hex.py
+++ HexaDecimal/Dec-Hex.py
@@ -4,15 +4,13 @@ decimal = int(input("Decimal: "))
 decimal_remainder = ""
 zero = False
 item = 0
-ListHexaDecimals = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+hexadecimal_table = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 print("Calculating...")
 
 while not(zero):
-  item = decimal % 16
+  decimal_remainder += hexadecimal_table[decimal % 16]  
   decimal = floor(decimal/16)
   if decimal == 0:
     zero = True
-
-  decimal_remainder += ListHexaDecimals[item]
 
 print("HexaDecimal: " + decimal_remainder[::-1])
