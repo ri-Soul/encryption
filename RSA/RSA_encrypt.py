eeaf8fdf925884059e9fadd84f92878120ba61f0
Just a pixel <91799709+ri-Soul@users.noreply.github.com>
2022-09-10 19:17:49 +0200

    la

--- RSA/RSA_encrypt.py
+++ RSA/RSA_encrypt.py
@@ -1,1 +1,13 @@
+import RSA
 
+def generate(bitsize):
+    (public_key, private_key) = rsa.newkeys(bitsize, poolsize = 8)
+    return public_key, private_key
+
+def encrypt(key, msg):
+    msg = str(msg).encode("utf-8")
+    return msg.encrypt(msg, key)
+    
+def decrypt(msg, key):
+    msg.decrypt(msg, key)
+    return msg.decode("utf-8")
